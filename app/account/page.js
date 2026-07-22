9bf281082734a015ed60d4104496127b0885f1ce
DreadTsx <hallowpahe@gmail.com>
2026-07-22 22:02:13 +0100

    more code

--- app/account/page.js
+++ app/account/page.js
@@ -1,5 +1,14 @@
+export const metadata = {
+  title: "Guest area",
+};
 function Page() {
-  return <h1>Account page</h1>;
+  return (
+    <div>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Welcome dread
+      </h2>
+    </div>
+  );
 }
 
 export default Page;
