5aca04726230343d9c8cdaf1afdbef8892f3015f
Dread <121462948+DreadTsx@users.noreply.github.com>
2026-07-25 19:29:28 +0000

    448. Streaming UI with Suspense: Cabin List

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,13 @@
-import CabinCard from "@/app/components/CabinCard";
-import { getCabins } from "../lib/data-service";
+import { Suspense } from "react";
+import CabinList from "@/app/components/CabinList";
+import Spinner from "@/app/components/Spinner";
 
 export const metadata = {
   title: "Cabins",
 };
 
-async function Page() {
-  const cabins = await getCabins();
+function Page() {
   
-
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -23,13 +22,9 @@ async function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner/>}>
+      <CabinList/>
+      </Suspense>
     </div>
   );
 }
